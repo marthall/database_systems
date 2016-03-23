3ae37ceec5be1c54bec4e1e54882683a37eaadbe
Martin Hallen <marthall@tsf-484-wpa-2-197.epfl.ch>
2016-03-23 16:14:00 +0100

    test

--- Project2/src/main/scala/Project1.scala
+++ Project2/src/main/scala/Project1.scala
@@ -19,19 +19,39 @@ object Project1 {
 
 	val tasknr = args(2)
 
-	val customers = spark.textFile(args(0) + "/customer.tbl").map(x => x.split('|'))
-	val orders = spark.textFile(args(0) + "/orders.tbl").map(x => x.split('|'))
+	val customers = spark.textFile(args(0) + "/customer.tbl").flatMap(x => {
+		val fields = x.split('|')
+		val customerKey = fields(0).toInt
+		List((customerKey, customerKey))
+	})
+	val orders = spark.textFile(args(0) + "/orders.tbl").flatMap(x => {
+		val fields = x.split('|')
+		val orderKey = fields(0).toInt
+		val customerKey = fields(1).toInt
+		val orderDetails = fields(8)
+		List((orderKey, customerKey, orderDetails))
+	})
+
+	// val customers = customers_raw.partitionBy(new org.apache.spark.HashPartitioner(200))
+	// val orders = orders_raw.partitionBy(new org.apache.spark.HashPartitioner(200))
+
+	println(customers.partitions.length)
+
+	// val customers = spark.parallelize(customers_raw, 10)
+	// val orders = spark.parallelize(orders_raw, 10)
 
 	// val customes_transformed = customers.cartesian(spark.parallelize(i <- 0 to N))
 
 	val regex = ".*special.*requests.*".r
 
-	val orderCount = orders.filter(x => !regex.pattern.matcher(x(8)).matches)
-							.map(order => (order(1), 1)).reduceByKey(_ + _)
+	val t0 = System.nanoTime()
 
-	val simpleCustomers = customers.map(customer => (customer(0),customer(0)))
+	val orderCount = orders.filter(x => !regex.pattern.matcher(x._3).matches)
+							.map(order => (order._2, 1)).reduceByKey(_ + _)
 
-	val customerOrders = simpleCustomers.join(orderCount)
+	// val simpleCustomers = customers.map(customer => (customer(0),customer(0)))
+
+	val customerOrders = customers.leftOuterJoin(orderCount)
 
 	val orderCustomers = customerOrders.map(x => (x._2._2, x._2._1))
 
@@ -39,7 +59,15 @@ object Project1 {
 
 	val result = orderCountCustomers.sortByKey()
 
-	val cleanResult = result.map(el => List(el).mkString("|"))
+	val cleanResult = result.map({
+		case (Some(a:Int), b) => List(a, b).mkString("|")
+		case (_, b) => List(0, b).mkString("|")
+	})
+
+	val t1 = System.nanoTime()
+	val seconds = (t1 - t0)/1000000000.0
+
+	println("Elapsed time: " + seconds + "seconds")
 
 	cleanResult.collect.foreach(println)
 	
@@ -51,7 +79,7 @@ object Project1 {
 		outpath = "/out_2"
 	}
 
-	cleanResult.saveAsTextFile(args(1) + outpath)			
+	cleanResult.saveAsTextFile(args(1) + outpath)		
 
     spark.stop()
   }
